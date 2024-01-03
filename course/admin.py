ca48282bd27ca4078c0d1598c8519d3501d1fa26
Majid Khamis <majid111111mm@gmail.com>
2024-01-03 14:52:00 +0400

    create models&admin , make migrations&migrate

--- course/admin.py
+++ course/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Category,Course,Review
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display=['program','math','scince','health']
+
+
+
+admin.site.register(Category,CategoryAdmin)
+admin.site.register(Course)
+admin.site.register(Review)
--- course/models.py
+++ course/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+# آقسام الدورات
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    # الدورة
+
+class Course(models.Model):
+    name = models.CharField(max_length=255)
+    subtitle = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+# ال
+class Review(models.Model):
+    course = models.ForeignKey(Course, related_name='reviews', on_delete=models.CASCADE)
